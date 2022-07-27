cb9a6cbd381bcd0c35e447e2e4fed1ffd7380a87
Lucas Martins Arruda <lucas.martins.fsa@gmail.com>
2022-07-27 14:10:36 +0000

    Update notificacaoFalha.sh

--- notificacaoFalha.sh
+++ notificacaoFalha.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -X POST "https://api.telegram.org/bot5314070423:AAHI2BES14YTY0bJtHOesbqNRMC6_C-c19s/sendMessage" -d "chat_id=-754470674&text=Pipeline executada com falhas!" 
+curl -X POST "https://api.telegram.org/bot5314070423:AAHI2BES14YTY0bJtHOesbqNRMC6_C-c19s/sendMessage" -d "chat_id=-754470674&text=Pipeline executada com falhas!"
+
+
+curl -H 'Content-Type: application/json' -d '{"text": "Pipeline executada com falhas!"}' https://ifgoias.webhook.office.com/webhookb2/3fedeecd-e5be-466b-9cf9-d078433981c8@951ff754-f2b4-4049-93a9-7a10f3b00d94/IncomingWebhook/9a61b57825874f239a6595da2c651bf8/c39d0fa2-39fd-46ae-b496-eeccbe13d910
